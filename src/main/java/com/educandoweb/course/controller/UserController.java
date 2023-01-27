433dc07aa972456e37222b3d1406a95967e8d0b5
ErickReis <erickrr98@gmail.com>
2023-01-27 07:53:22 -0300

    Implementação dos metodos findAll e findById da entidade User

--- src/main/java/com/educandoweb/course/controller/UserController.java
+++ src/main/java/com/educandoweb/course/controller/UserController.java
@@ -1,21 +1,35 @@
 package com.educandoweb.course.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.service.UserService;
+
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "11987654321", "123456");
-		return ResponseEntity.ok().body(u);
-		
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = userService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		return ResponseEntity.ok().body(userService.findById(id));
 	}
 	
 }
