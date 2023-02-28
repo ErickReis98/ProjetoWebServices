78a6172d56da9741681f4ca682fcbd0b3ce76547
ErickReis <erickrr98@gmail.com>
2023-02-27 22:18:12 -0300

    Inserindo o metodo para excluir User via JSON

--- src/main/java/com/educandoweb/course/controller/UserController.java
+++ src/main/java/com/educandoweb/course/controller/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,5 +41,11 @@ public class UserController {
 	@ResponseStatus(value = HttpStatus.CREATED)
 	public ResponseEntity<User> insert(@RequestBody User user) {
 		return ResponseEntity.ok().body(userService.insert(user));
+	}
+	@Transactional
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
